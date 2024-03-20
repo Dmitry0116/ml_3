bdb807824f9afcef5845c0b8f0695b0593500c97
Dmitry0116 <155762834+Dmitry0116@users.noreply.github.com>
2024-03-20 23:42:59 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -6,15 +6,20 @@ from pydantic import BaseModel
 class Item(BaseModel):
     text: str
 
+
 app = FastAPI()
 clf = pipeline(
-    task = 'text-classification', 
-    model = 'cointegrated/rubert-tiny2-cedr-emotion-detection')
+    task ='text-classification',
+    model ='cointegrated/rubert-tiny2-cedr-emotion-detection')
 @app.get("/")
+
+
 def root():
     return {"message": "Hello World"}
 
 @app.post("/predict/")
+
+
 def predict(item: Item):
     """Распознование эмоций в тексте на русском языке"""
     return clf(item.text)
