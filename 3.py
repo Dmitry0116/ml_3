4bed46eedbfceaeafa47b8543990e83a12fba517
Dmitry0116 <155762834+Dmitry0116@users.noreply.github.com>
2024-03-21 00:02:01 +0500

    3.py

--- 3.py
+++ 3.py
@@ -9,17 +9,16 @@ class Item(BaseModel):
 
 app = FastAPI()
 clf = pipeline(
-    task ='text-classification',
-    model ='cointegrated/rubert-tiny2-cedr-emotion-detection')
+    task="text-classification", model="cointegrated/rubert-tiny2-cedr-emotion-detection"
+)
+
+
 @app.get("/")
-
-
 def root():
     return {"message": "Hello World"}
 
+
 @app.post("/predict/")
-
-
 def predict(item: Item):
     """Распознование эмоций в тексте на русском языке"""
     return clf(item.text)
